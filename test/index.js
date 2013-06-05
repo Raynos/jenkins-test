b5831d61f3c5d41167f24c57cac2ed9ec472e50f
Raynos <raynos2@gmail.com>
2013-06-04 18:42:23 -0600

    Update index.js

--- test/index.js
+++ test/index.js
@@ -4,5 +4,6 @@ var jenkinsTest = require("../index")
 
 test("jenkinsTest is a function", function (assert) {
     assert.equal(typeof jenkinsTest, "function")
+    assert.equal(jenkinsTest.hello, "hello")
     assert.end()
 })
